96f3cd25d4c26077726bd47e7692a4f42fc9b337
nikita <twothousandvampires@gmail.com>
2022-05-19 16:46:58 +0300

    'nwm'

--- src/components/SkillTree.vue
+++ src/components/SkillTree.vue
@@ -6,12 +6,16 @@ export default {
   },
   data(){
     return{
-      page : 'combat'
+      page : 'combat',
+      clicked_skill : undefined
     }
   },
   methods:{
     setPage(page){
       this.page = page
+    },
+    skillClick(item){
+      this.clicked_skill = item
     }
   }
 
@@ -19,6 +23,26 @@ export default {
 </script>
 
 <template>
+  <div v-if="this.clicked_skill" id="skill_modal">
+    <div id="skill_main">
+      <div id="skill_main_info">
+        <div id="skill_img">
+          <img :src=this.clicked_skill.img_path alt="">
+        </div>
+        <div id="skill_info">
+          <p>{{this.clicked_skill.name}}</p>
+          <p>{{this.clicked_skill.description}}</p>
+
+        </div>
+      </div>
+      <div id="skill_main_level_status">
+        <p>Текущий уровень {{this.clicked_skill.level}}</p>
+      </div>
+    </div>
+    <div id="skill_childs">
+
+    </div>
+  </div>
   <div id="tree_wrap">
     <div id="page">
       <div id="menu">
@@ -27,8 +51,8 @@ export default {
         <p @click="setPage('travel')">Travel</p>
         <p @click="setPage('active')">Active</p>
       </div>
-      <div>
-        <div v-for="(item) in char.skill_tree.getPassives(this.page)">
+      <div id="skills">
+        <div @click="skillClick(item)" id="skill" v-for="(item) in char.skill_tree.getPassives(this.page)">
           <p>{{item.name}}</p>
           <img :src=item.img_path alt="">
         </div>
@@ -38,7 +62,56 @@ export default {
 </template>
 
 <style scoped>
+  #skill_main{
+    display: flex;
+    flex-direction: row;
+  }
+  #skill_main_info{
+    display: flex;
+    flex-direction: row;
+  }
+  #skill_main_info img {
+    width: 100px;
+    height: 100px;
+  }
+  #skill_modal{
+    background-color: white;
+    border: 5px solid #40c4c8;
+    border-image: url('/src/assets/img/border/border_big.png') 5 stretch stretch;
+    color: black;
+    position: absolute;
+    z-index: 100000;
+    margin-top: auto;
+    margin-right: auto;
+  }
+  #skill{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  #skill img {
+    width: 70px;
+    height: 70px;
+  }
+  #skills{
+    padding: 20px;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-end;
+    justify-content: space-around;
+  }
+  #menu{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+  #menu p {
+    cursor: pointer;
+  }
   #page{
+    display: flex;
+    flex-direction: row;
     background-color: #c8c8c8;
     width: 100%;
   }
